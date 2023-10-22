cd99b7c5f2e88ab49cbf48f6ea8afd170ebcb84e
brayand227 <brayancalle227@gmail.com>
2023-10-22 17:29:33 -0500

    main

--- src/main/java/co/edu/uniquindio/poo/App.java
+++ src/main/java/co/edu/uniquindio/poo/App.java
@@ -1,19 +1,22 @@
 package co.edu.uniquindio.poo;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
+import java.util.function.Predicate;
 
 class Biblioteca {
-     String nombre;
-     String direccion;
-     int cantidadLibros;
-     String fechaCreacion;
-     private Set<String> nombresLibros;
+    private String nombre;
+    private String direccion;
+    private int cantidadLibros;
+    private String fechaCreacion;
+    private Set<Libro> nombresLibros;
 
-    public Biblioteca(String nombre, String direccion, int cantidadLibros, String fechaCreacion) {
+    public Biblioteca(String nombre, String direccion, String fechaCreacion) {
         this.nombre = nombre;
         this.direccion = direccion;
-        this.cantidadLibros = cantidadLibros;
         this.fechaCreacion = fechaCreacion;
         nombresLibros = new HashSet<>();
     }
@@ -37,7 +40,8 @@ class Biblioteca {
     }
 
     public int getCantidadLibros() {
-        return cantidadLibros;
+
+        return nombresLibros.size();
     }
 
     public void setCantidadLibros(int cantidadLibros) {
@@ -52,19 +56,118 @@ class Biblioteca {
         this.fechaCreacion = fechaCreacion;
     }
 
+    public boolean BuscarLibroAutor(Libro nn) {
+        boolean centinela = false;
+        for (Libro i : nombresLibros) {
+            if (i.getAutor().equals(nn.getAutor())) {
+                centinela = true;
+            }
+
+        }
+
+        return centinela;
+    }
+
+    public Optional<Libro> BuscarLibrosPorFormato(LOibroDigital lOibroDigital) {
+        Predicate<Libro> nombreIgual = j -> j.getAutor().equals(lOibroDigital.getAutor());
+        return nombresLibros.stream().filter(nombreIgual).findAny();
+
+    }
+
+    public Optional<Libro> LibrosDigitalesconVersionImpresa(LOibroDigital lOibroDigital) {
+        Predicate<Libro> nombreIgual = j -> j.getAutor().equals(lOibroDigital.getAutor());
+        return nombresLibros.stream().filter(nombreIgual).findAny();
+
+    }
+
+    public Optional<Libro> BuscarLibroPorNombre(String nombre) {
+        Predicate<Libro> nombreIgual = j -> j.getNombreLibro().equals(nombre);
+        return nombresLibros.stream().filter(nombreIgual).findAny();
+    }
+
+    public Collection<LibroImpreso> ListaLibrosImpresos() {
+        Predicate<Libro> condicion = libro -> libro instanceof LibroImpreso;
+        return nombresLibros.stream().filter(condicion).map(libro -> (LibroImpreso) libro).toList();
+
+    }
+
+    public Collection<LibroCd> ListaLibrosCd() {
+        Predicate<Libro> condicion = libro -> libro instanceof LibroCd;
+        return nombresLibros.stream().filter(condicion).map(libro -> (LibroCd) libro).toList();
+
+    }
+
+    public Collection<LOibroDigital> ListaLibrosDigital() {
+        Predicate<Libro> condicion = libro -> libro instanceof LOibroDigital;
+        return nombresLibros.stream().filter(condicion).map(libro -> (LOibroDigital) libro).toList();
+    }
+
     public void mostrarInformacion() {
         System.out.println("Nombre: " + nombre);
         System.out.println("Dirección: " + direccion);
         System.out.println("Cantidad de libros: " + cantidadLibros);
         System.out.println("Fecha de creación: " + fechaCreacion);
-        System.out.println(agregarLibro(libroA));
-        System.out.println(agregarLibro(libroB));
+        // System.out.println(agregarLibro(libroA));
+        // System.out.println(agregarLibro(libroB));
         System.out.println(nombresLibros);
     }
 
-    public boolean agregarLibro(LibroImpreso libro) {
+    public int CantidadLibrosPorDigitales(Autor autor) {
+        assert autor != null;
+        int contadorLibrosDigitales = 0;
+
+        Collection<LOibroDigital> libroDigital = ListaLibrosDigital();
+
+        for (LOibroDigital i : libroDigital) {
+            if (i.getAutor().equals(autor)) {
+                contadorLibrosDigitales++;
+            }
+        }
+
+        return contadorLibrosDigitales;
+    }
+
+    public int CantidadLibrosPorCd(Autor autor) {
+        assert autor != null;
+        int contadorLibrosCd = 0;
+
+        Collection<LibroCd> libroCds = ListaLibrosCd();
+        for (LibroCd i : libroCds) {
+            if (i.getAutor().equals(autor)) {
+                contadorLibrosCd++;
+            }
+        }
+
+        return contadorLibrosCd;
+    }
+
+    public int CantidadLibrosImpresos(Autor autor) {
+        assert autor != null;
+        int contadorLibrosImpresos = 0;
+
+        Collection<LibroImpreso> libroImpresos = ListaLibrosImpresos();
+        for (LibroImpreso i : libroImpresos) {
+            if (i.getAutor().equals(autor)) {
+                contadorLibrosImpresos++;
+            }
+        }
+
+        return contadorLibrosImpresos;
+    }
+
+    public ArrayList<Integer> cantidadLibrosAutor(Autor autor) {
+        assert autor != null;
+        ArrayList<Integer> cant = new ArrayList<>();
+        cant.add(CantidadLibrosImpresos(autor));
+        cant.add(CantidadLibrosPorCd(autor));
+        cant.add(CantidadLibrosPorDigitales(autor));
+
+        return cant;
+    }
+
+    public boolean agregarLibro(Libro libro) {
         if (!nombresLibros.contains(libro.getNombreLibro())) {
-            nombresLibros.add(libro.getNombreLibro());
+            nombresLibros.add(libro);
             System.out.println("El libro ha sido agregado exitosamente.");
             return true;
         } else {
@@ -72,13 +175,5 @@ class Biblioteca {
             return false;
         }
     }
-    LibroImpreso libroA = new LibroImpreso("La divina comedia","1472","Dante Aleghieri","Grupo Planeta");
-    LibroImpreso libroB = new LibroImpreso("El Resplandor","1977","Stephen King","Grupo Planeta");
-        
-
 
 }
-
-
-
-
--- src/main/java/co/edu/uniquindio/poo/LibroImpreso.java
+++ src/main/java/co/edu/uniquindio/poo/LibroImpreso.java
@@ -3,17 +3,19 @@ package co.edu.uniquindio.poo;
 public class LibroImpreso extends Libro {
 
     private String nombre;
+
+    public LibroImpreso(String nombreLibro, String fechaCreacion, Autor autor, Editorial editorial, String nombre,
+            String fecha, Autor autor2, Editorial editorial2) {
+        super(nombreLibro, fechaCreacion, autor, editorial);
+        this.nombre = nombre;
+        this.fecha = fecha;
+        autor = autor2;
+        editorial = editorial2;
+    }
+
     private String fecha;
     private Autor autor;
     private Editorial editorial;
-
-    public LibroImpreso(String nombre, String fecha, Autor autor, Editorial editorial){
-        this.nombre = nombre;
-        this.fecha = fecha;
-        this.autor = autor;
-        this.editorial = editorial;
-
-    }
 
     public String getNombre() {
         return nombre;
@@ -31,11 +33,4 @@ public class LibroImpreso extends Libro {
         return editorial;
     }
 
-    
-
-
-
-
- 
-    
 }
